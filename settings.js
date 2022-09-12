0cc5a4710e9c7cf64b11e012d3a883468e797676
tharuka2009 <113086262+tharuka2009@users.noreply.github.com>
2022-09-12 07:07:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,15 +12,15 @@ global.APIKeys = {
 }
 
 
-global.OWNER_REACT = '🧑🏻‍💻'
-global.REACT_NUMBER = '94714188730'
+global.OWNER_REACT =🏻‍💻'🔛'
+global.REACT_NUMBER = '94740925284'
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
-global.available = true //auto available (true to on, false to off)
+global.autoTyping = ture //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = ture //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = ture//auto reading in gc (true to on, false to off)
+global.autoRecord = ture //auto recording (true to on, false to off)
+global.available = ture //auto available (true to on, false to off)
 
 //semt menu variants
 global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
@@ -34,32 +34,32 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "NIPUNA " //ur owner name
-global.ytname = "YT: Nipuna-apps" //ur yt chanel name
+global.vcardowner = ['94740925284'] //ur owner number
+global.ownername = "Tharuksh" //ur owner name
+global.ytname = "YT: Tharuksha officia Video" //ur yt chanel name
 global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['714188730']
-global.ownertag = '714188730' //ur tag number
-global.botname = '⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜' //ur bot name
-global.linkz = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //your theme url which will be displayed on whatsapp
+global.owner = ['740925284']
+global.ownertag = '740925284' //ur tag number
+global.botname = '⚜𝐓𝐡𝐚𝐫𝐮𝐤𝐬𝐡𝐚 𝐘𝐓⚜' //ur bot name
+global.linkz = "https://chat.whatsapp.com/IUSH3E3lyPhLiTex4e4EEc" //your theme url which will be displayed on whatsapp
 global.websitex = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //ur website to be displayed
 global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot.git' //script link
-global.reactmoji = "❤" //ur menu react emoji
+global.reactmoji = "⚠️" //ur menu react emoji
 global.themeemoji = "🎸" //ur theme emoji
-global.packname = "⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜ Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "NIPUNA" //ur sticker watermark author
+global.packname = "⚜𝐓𝐡𝐚𝐫𝐮𝐤𝐬𝐡𝐚 𝐘𝐓⚜ Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "𝐓𝐡𝐚𝐫𝐮𝐤𝐬𝐡𝐚" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync(".https://telegra.ph/file/de29674cca9c7dcec0555.jpg") //ur thumb pic
+global.log0 = fs.readFileSync(".https://telegra.ph/file/de29674cca9c7dcec0555.jpg") //ur logo pic
+global.err4r = fs.readFileSync(".https://telegra.ph/file/de29674cca9c7dcec0555.jpg") //ur error pic
+global.thumb = fs.readFileSync(".https://telegra.ph/file/de29674cca9c7dcec0555.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['714188730'] //ur premium numbers
+global.premium = ['766322316'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
